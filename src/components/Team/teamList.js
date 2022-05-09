48a8b9d366fd351d10e22850aa5ba5c33a75be6b
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2022-05-09 13:35:34 -0300

    team

--- src/components/Team/teamList.js
+++ src/components/Team/teamList.js
@@ -20,24 +20,6 @@ const teamList = [
     description:
       "Experto en políticas públicas digitales y consultor de distintas entidades del sector público y privado. Actualmente es CEO de SmC+ Digital Public Affairs, y asesor de distintas empresas y organizaciones como el BID, la Asociación Latinoamericana de Internet (ALAI). También, se desempeña como investigador afiliado del Centro de Tecnología y Sociedad (CETyS) de la Universidad de San Andrés, Argentina. Fue el jefe de la Asociación GSMA que reúne a todo el ecosistema móvil entre 2010 y 2018, y miembro del Comité de Internet para Todos del Foro Económico Mundial (WEF) y ha liderado iniciativas regionales de diálogo público-privado como el Congreso Latinoamericano de Telecomunicaciones (CLT) el centro de capacitación CE-Digital, la campaña Nos Importa y la red BEST, entre otros. Tecnológico de Buenos Aires (ITBA).",
   },
-  {
-    image: sebas,
-    name: "Sebastián Cabello",
-    description:
-      "Experto en políticas públicas digitales y consultor de distintas entidades del sector público y privado. Actualmente es CEO de SmC+ Digital Public Affairs, y asesor de distintas empresas y organizaciones como el BID, la Asociación Latinoamericana de Internet (ALAI). También, se desempeña como investigador afiliado del Centro de Tecnología y Sociedad (CETyS) de la Universidad de San Andrés, Argentina. Fue el jefe de la Asociación GSMA que reúne a todo el ecosistema móvil entre 2010 y 2018, y miembro del Comité de Internet para Todos del Foro Económico Mundial (WEF) y ha liderado iniciativas regionales de diálogo público-privado como el Congreso Latinoamericano de Telecomunicaciones (CLT) el centro de capacitación CE-Digital, la campaña Nos Importa y la red BEST, entre otros. Tecnológico de Buenos Aires (ITBA).",
-  },
-  {
-    image: diego,
-    name: "Diego Ross Rooney",
-    description:
-      "Experto en políticas públicas digitales y consultor de distintas entidades del sector público y privado. Actualmente es CEO de SmC+ Digital Public Affairs, y asesor de distintas empresas y organizaciones como el BID, la Asociación Latinoamericana de Internet (ALAI). También, se desempeña como investigador afiliado del Centro de Tecnología y Sociedad (CETyS) de la Universidad de San Andrés, Argentina. Fue el jefe de la Asociación GSMA que reúne a todo el ecosistema móvil entre 2010 y 2018, y miembro del Comité de Internet para Todos del Foro Económico Mundial (WEF) y ha liderado iniciativas regionales de diálogo público-privado como el Congreso Latinoamericano de Telecomunicaciones (CLT) el centro de capacitación CE-Digital, la campaña Nos Importa y la red BEST, entre otros. Tecnológico de Buenos Aires (ITBA).",
-  },
-  {
-    image: mauri,
-    name: "Mauricio Fernandéz",
-    description:
-      "Experto en políticas públicas digitales y consultor de distintas entidades del sector público y privado. Actualmente es CEO de SmC+ Digital Public Affairs, y asesor de distintas empresas y organizaciones como el BID, la Asociación Latinoamericana de Internet (ALAI). También, se desempeña como investigador afiliado del Centro de Tecnología y Sociedad (CETyS) de la Universidad de San Andrés, Argentina. Fue el jefe de la Asociación GSMA que reúne a todo el ecosistema móvil entre 2010 y 2018, y miembro del Comité de Internet para Todos del Foro Económico Mundial (WEF) y ha liderado iniciativas regionales de diálogo público-privado como el Congreso Latinoamericano de Telecomunicaciones (CLT) el centro de capacitación CE-Digital, la campaña Nos Importa y la red BEST, entre otros. Tecnológico de Buenos Aires (ITBA).",
-  },
 ];
 
 export default teamList;
