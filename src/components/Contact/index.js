b608d6d141ddc6d441802898a67bbaae8b05df8d
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2022-05-09 15:01:10 -0300

    contact-form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,30 @@
 import React from "react";
 import imageContact from "../../assets/contactimage.webp";
 import "./Contact.scss";
+import emailjs from "@emailjs/browser";
+import toast, { Toaster } from "react-hot-toast";
 const Contact = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_136b6f5",
+        "template_4p3lzqi",
+        e.target,
+        "Ydazqgf9SjOwUG1TN"
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          toast.success("¡Consulta enviada correctamente!");
+          return;
+        }
+        throw new Error("Ocurrio un error");
+      })
+      .catch((error) => {
+        toast.error("Ocurrio un error. Intente nuevamente.");
+      });
+  };
+
   return (
     <div className="app__contact-container" id="contacto">
       <div className="app__contact-container-image">
@@ -10,15 +33,16 @@ const Contact = () => {
       <div className="app__contact-container-data">
         <div className="app__contact-container-form">
           <h2>Contact</h2>
-          <form className="app__contact-form">
-            <input placeholder="Nombre" />
-            <input placeholder="Email" />
-            <textarea placeholder="Consulta"></textarea>
+          <form className="app__contact-form" onSubmit={onSubmit}>
+            <input placeholder="Nombre" name="user_name" />
+            <input placeholder="Email" name="user_email" />
+            <textarea placeholder="Consulta" name="message"></textarea>
             <div></div>
-            <input type="submit" value="Enviar" />
+            <input type="submit" value="Enviar" style={{ cursor:"pointer" }}/>
           </form>
         </div>
       </div>
+      <Toaster />
     </div>
   );
 };
