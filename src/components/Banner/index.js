f27f8c0d8f970fef9d09c39a59be7716441f5f31
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2022-06-08 02:39:02 -0300

    changes

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -7,20 +7,25 @@ const Banner = () => {
   return (
     <motion.div
       className="banner"
-      animate={{ opacity: [1, 0], display:"none" }}
-      transition={{ default:{ duration:0.3, delay:1 }, display:{ delay:1.5 } }}
+      animate={{ opacity: [1, 0], display: "none" }}
+      transition={{
+        default: { duration: 0.3, delay: 1 },
+        display: { delay: 1.5 },
+      }}
     >
-      <LogoBanner />
-      <motion.div
-        style={{ marginTop: "30px" }}
-        animate={{ opacity: [1, 0] }}
-        transition={{
-          duration: 0.4,
-          repeat:"Infinity"
-        }}
-      >
-        <TableLoader />
-      </motion.div>
+      <div>
+        <LogoBanner />
+        <motion.div
+          style={{ marginTop: "30px" }}
+          animate={{ opacity: [1, 0] }}
+          transition={{
+            duration: 0.4,
+            repeat: "Infinity",
+          }}
+        >
+          <TableLoader />
+        </motion.div>
+      </div>
     </motion.div>
   );
 };
